9b67c9994d163cff36697680545bf75b6fe071fd
Covenant University <madeniran.2300324@stu.cu.edu.ng>
2026-06-05 15:05:47 +0100

    fixes

--- app/core/config.py
+++ app/core/config.py
@@ -19,7 +19,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
     
     APP_ENV:  str = "development"
     APP_PORT: int = 8001
